a1389f3bf1aeb54b9d2efc6bbff6ee87ff79452c
Thanadon Chutrakoon <104269994+fietao@users.noreply.github.com>
2026-04-01 08:29:31 -0500

    Enhance Battle Simulation with character customization
    Added character customization and improved battle logic.

--- BattleSimulation.java
+++ BattleSimulation.java
@@ -1,44 +1,70 @@
 import java.util.Random;
+import java.util.Scanner;
 public class BattleSimulation {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Random rand = new Random();
+        char choice;
+         String name1 , name2;
+        int strength1, strength2, speed1, speed2;
+        System.out.println("Welcome to the Battle Simulator!");
+        choice = scanner.next().charAt(0);
+        System.out.println("do you want to customize your characters? (y/n)");
+            if(choice == 'y' || choice =='Y'){
+            System.out.println("Name your character 1");
+            scanner.nextLine();
+            name1 = scanner.nextLine();
+            System.out.println("put your character 1 strength (0-5)");
+
+
+
+            }
+        else{}
         Character character1 = new Character("Jet", 3, 2);
         Character character2 = new Character("V", 4, 1);
+
         System.out.println("It's a dual to the death. Let's welcome our opponents:");
-        System.out.println(character1.name + " has " + character1.getHealth() + " health.");
-        System.out.println(character2.name + " has " + character2.getHealth() + " health.");
+        System.out.println(character1.getName() + " has " + character1.getCurrentHealth() + " health.");
+        System.out.println(character2.getName() + " has " + character2.getCurrentHealth() + " health.");
+            scanner.nextLine();
             System.out.println("Let the battle begin!");
-            System.out.println("***************************************");
-            System.out.println(character1.getHealth());
-            System.out.println(character2.getHealth());
-            while(character1.getHealth() > 0 && character2.getHealth() > 0){
+            System.out.println("***************************************");        
+            while(character1.getCurrentHealth() > 0 && character2.getCurrentHealth() > 0){
                 if(rand.nextDouble() < 0.7){
-                    
+                    int damage1 = character1.attack(character2);
+                    System.out.println(character1.getName() + " attacks! make "+ damage1+" damage to V");
+                        if(character2.IsAlive() == false){
+                        System.out.println("Current health " + character1.getName() + ": " + character1.getCurrentHealth());
+                        System.out.println("Current health " + character2.getName() + ": " + character2.getCurrentHealth());
+                        System.out.println(character1.getName() + " has  defeat " + character2.getName() + " " + character1.getName() + " wins!"); break;
+                        
+                        
+                        }
+                        
+                    int damage2 = character2.attack(character1);
+                    System.out.println(character2.getName() + " attacks! make "+ damage2+" damage to Jet");
+                        if(character1.IsAlive() == false){
+                        System.out.println("Current health " + character1.getName() + ": " + character1.getCurrentHealth());
+                        System.out.println("Current health " + character2.getName() + ": " + character2.getCurrentHealth());
+                            System.out.println(character2.getName() + " has defeat " + character1.getName() + " " + character2.getName() + " wins!"); break;
+                        }
+                    System.out.println("Current health " + character1.getName() + ": " + character1.getCurrentHealth());
+                    System.out.println("Current health " + character2.getName() + ": " + character2.getCurrentHealth());
+                    scanner.nextLine();
                 }
                 else{
-                
+                    int heal1 = character1.heal();
+                    System.out.println(character1.getName() + " Heal! Gained " + heal1 + " health.");
+                    int heal2 = character2.heal();
+                    System.out.println(character2.getName() + " Heal! Gained " + heal2 + " health.");
+                    System.out.println("Current health " + character1.getName() + ": " + character1.getCurrentHealth());
+                    System.out.println("Current health " + character2.getName() + ": " + character2.getCurrentHealth());
+                scanner.nextLine();
                 }
-                System.out.println("Current health: " + character1.name + ": " + character1.getHealth());
-                System.out.println("Current health: " + character2.name + ": " + character2.getHealth());
+               
+              
             }
         }
-            /*while(character1.CurrentHealth > 0 && character2.CurrentHealth > 0){
-                if(rand.nextDouble() < 0.7){
-                    character1.CurrentHealth -= character2.attack;
-                    System.out.println(character2.name + " attacks!"+ character2.damage);
-                    character2.CurrentHealth -= character1.attack;
-                    System.out.println(character1.name + " attacks!"+ character1.damage);
-                }
-                else{
-                    character2.CurrentHealth += character2.heal;
-                    System.out.println(character2.name + " Heal!");
-                    character1.CurrentHealth += character1.heal;
-                    System.out.println(character1.name + " Heal!");
-                }
-                System.out.println("curent health:" + character1.name + ": " + character1.getHealth());
-                System.out.println("curent health:" + character2.name + ": " + character2.getHealth());
-                System.out.println("-------------------");
-            }
-        } */
+        
     }
     
