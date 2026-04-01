e55f6a3e0d0e85c0689673b960ff79d2fbd3a66f
Jet <jetzazaver400@gmail.com>
2026-03-31 22:42:48 -0500

    Update battle simulation and character behavior

--- BattleSimulation.java
+++ BattleSimulation.java
@@ -5,39 +5,40 @@ public class BattleSimulation {
         Character character1 = new Character("Jet", 3, 2);
         Character character2 = new Character("V", 4, 1);
         System.out.println("It's a dual to the death. Let's welcome our opponents:");
-        System.out.println(character1.name + " has " + character1.CurrentHealth + " health.");
-        System.out.println(character2.name + " has " + character2.CurrentHealth + " health.");
-        System.out.println("Let the battle begin!");
-        System.out.println("***************************************");}
-
-
-
-
-
-        /**while(character1.CurrentHealth > 0 && character2.CurrentHealth > 0){
-            if(rand.nextDouble() < 0.7){
-                character1.CurrentHealth -= character2.attack;
-                System.out.println(character2.Name + " attacks!"+ character2.damage)
-                character2.CurrentHealth -= character1.attack;
-                System.out.println(character2.Name + " attacks!"+ character2.damage)
+        System.out.println(character1.name + " has " + character1.getHealth() + " health.");
+        System.out.println(character2.name + " has " + character2.getHealth() + " health.");
+            System.out.println("Let the battle begin!");
+            System.out.println("***************************************");
+            System.out.println(character1.getHealth());
+            System.out.println(character2.getHealth());
+            while(character1.getHealth() > 0 && character2.getHealth() > 0){
+                if(rand.nextDouble() < 0.7){
+                    
+                }
+                else{
+                
+                }
+                System.out.println("Current health: " + character1.name + ": " + character1.getHealth());
+                System.out.println("Current health: " + character2.name + ": " + character2.getHealth());
             }
-            else(){
-                character2.Name += character2.heal;
-                System.out.println(character2.Name + " Heal!")
-                character1.Name += character1.heal;
-                System.out.println(character1.Name + " Heal!")
-            }
-            System.out.println("curent health:" + character1.Name + ": " + character1.CurrentHealth);
-            System.out.println("curent health:" + character2.Name + ": " + character2.CurrentHealth);
-            System.out.println("-------------------");
-            */
-
-        
-            
-
-
-
-
         }
-
+            /*while(character1.CurrentHealth > 0 && character2.CurrentHealth > 0){
+                if(rand.nextDouble() < 0.7){
+                    character1.CurrentHealth -= character2.attack;
+                    System.out.println(character2.name + " attacks!"+ character2.damage);
+                    character2.CurrentHealth -= character1.attack;
+                    System.out.println(character1.name + " attacks!"+ character1.damage);
+                }
+                else{
+                    character2.CurrentHealth += character2.heal;
+                    System.out.println(character2.name + " Heal!");
+                    character1.CurrentHealth += character1.heal;
+                    System.out.println(character1.name + " Heal!");
+                }
+                System.out.println("curent health:" + character1.name + ": " + character1.getHealth());
+                System.out.println("curent health:" + character2.name + ": " + character2.getHealth());
+                System.out.println("-------------------");
+            }
+        } */
+    }
     
--- Character.java
+++ Character.java
@@ -1,23 +1,88 @@
+import java.util.Random;
+
 public class Character {
-    final int StartingHealth = 50;
-    public String name;
-    private boolean IsAlive;
-    private boolean tooManyPoints = false;
-    public int CurrentHealth = StartingHealth;
-    private int Health;
+
+    //Attributes
+    public static final int STARTING_HEALTH =50;
+    private static final int BASE_ATTACK = 10;
+    private static final int BASE_HEAL = 5;
+
+    public  int CurrentHealth ;
     private int Strength;
     private int Speed;
+    public String name;
 
 
+
+//    private boolean IsAlive;
+    private boolean tooManyPoints = false;
+//    private static int Health=5;
+//    private boolean critical_attack ;
+    private static boolean endGame ;
+
+
+    //Constructor
     public Character(String name, int strength, int speed) {
-        this.name = name;
         this.Strength = strength;
         this.Speed = speed;
-        if(Strength + Speed == 5 ){
+        this.name = name;
+        this.CurrentHealth = STARTING_HEALTH;
+
+        if(Strength + Speed > 5 ){
             tooManyPoints = true;
         }
     }
+    //Methods
+    Random rnd = new Random();
+    public int attack( Character opponent ){
 
+        int chance= rnd.nextInt(10)+1;
+       int Damage = BASE_ATTACK+Strength;
+        if (chance==1){
+            Damage +=5;
+        }
+        opponent.takeDamage(Damage);
+        return Damage;
+    }
+
+    public int heal(){
+       int healthGained = (2*Speed)+BASE_HEAL;
+       if (healthGained >STARTING_HEALTH){
+        healthGained=STARTING_HEALTH;
+       }
+        return healthGained;
+    }
+
+    public void takeDamage (int Damage){
+        CurrentHealth-=Damage;
+    }
+
+    //Several get methods
+
+
+    public int getStrength() {
+        return Strength;
+    }
+
+    public int getSpeed() {
+        return Speed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return CurrentHealth;
+    }
+
+    public String toString(){
+        return name + " [ Strength: " + Strength + ", Speed: " + Speed + " ] ";
+
+    }
 
 
 }
+
+
+
